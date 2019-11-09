bc35959a46cae579cbc2d922d836a8ad5bd213cd
Minh Pham <mqpham848@gmail.com>
2019-11-09 00:02:58 -0500

    working on user-auth

--- models/User.js
+++ models/User.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const bcrypt = require(bcrypt),
 const SALT = 10;
+const LOGIN_ATTEMPT = 5,
+const LOCK_TIME = 2 * 30 * 30 * 1000;
 
 const UserSchema = new Schema({
 
@@ -34,36 +36,33 @@ const UserSchema = new Schema({
         type: Number, 
         required: true, 
         default: 0}, 
-        lockUntil: { type: Number },
-    lockUntil: { type: Number }
+        lockUntil: { type: Number }
 }); 
 
-UserSchema.statics.failedLogin = {
-    NOT_FOUND: 0,
-    PASSWORD_INCORRECT: 1,
-    MAX_ATTEMPTS: 2
-};
+UserSchema.virtual('isLocked').get(function() {
+    // check for a future lockUntil timestamp
+    return !!(this.lockUntil && this.lockUntil > Date.now());
+});
 
-UserSchema.pre(save, function(next) {
-    var user = this;
+UserSchema.pre('save', function(next) {
+    let user = this;
 
 // only hash the password if it has been modified (or is new)
 if (!user.isModified('password')) return next();
 
-// generate a salt
-bcrypt.genSalt(SALT, function(err, salt) {
-    if (err) return next(err);
-
-    // hash the password using our new salt
-    bcrypt.hash(user.password, salt, function(err, hash) {
+    // generate a salt
+    bcrypt.genSalt(SALT, function(err, salt) {
         if (err) return next(err);
 
-        // override the cleartext password with the hashed one
-        user.password = hash;
-        next();
-    });
-});
+        // hash the password using our new salt
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            if (err) return next(err);
 
+            // override the cleartext password with the hashed one
+            user.password = hash;
+            next();
+        });
+    });
 });
 
 UserSchema.methods.comparePassword = function(candidatePassword, cb) {
@@ -71,6 +70,29 @@ UserSchema.methods.comparePassword = function(candidatePassword, cb) {
         if (err) return cb(err);
         cb(null, isMatch);
     });
+};
+
+UserSchema.methods.incLoginAttempts = function(cb) {
+    // if we have a previous lock that has expired, restart at 1
+    if (this.lockUntil && this.lockUntil < Date.now()) {
+        return this.update({
+            $set: { loginAttempts: 1 },
+            $unset: { lockUntil: 1 }
+        }, cb);
+    }
+    // otherwise we're incrementing
+    var updates = { $inc: { loginAttempts: 1 } };
+    // lock the account if we've reached max attempts and it's not locked already
+    if (this.loginAttempts + 1 >= MAX_LOGIN_ATTEMPTS && !this.isLocked) {
+        updates.$set = { lockUntil: Date.now() + LOCK_TIME };
+    }
+    return this.update(updates, cb);
+};
+
+let reasons = UserSchema.statics.failedLogin = {
+    NOT_FOUND: 0,
+    PASSWORD_INCORRECT: 1,
+    MAX_ATTEMPTS: 5
 };
 
 module.exports = User = mongoose.model('User', UserSchema);
