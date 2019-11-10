fe4f3395e7e65e8867b6eab3af315208fa0dc063
Minh Pham <mqpham848@gmail.com>
2019-11-10 13:11:23 -0500

    added Login & Signup froms

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import LoginPage from './components/pages/Login/Login';
-import RegisterForm from './components/pages/Register/Register';
 import Home from './components/pages/Home/Home';
 import Forum from './components/pages/Forum/Forum';
 import Pictures from './components/pages/Pictures';
@@ -13,8 +11,6 @@ class App extends Component {
       <Router>
         <div>
           <Switch>
-            <Route exact path='/Register' component={RegisterForm} />
-            <Route exact path='/Login' component={LoginPage} />
             <Route exact path='/' component={Home} />
             <Route exact path='/forum' component={Forum} />
             <Route exact path='/pictures' component={Pictures} />
