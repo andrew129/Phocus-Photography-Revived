1659fada83c1113336e232787ac1a4754447b28c
Andrew Stiles <ndrwstls544@gmail.com>
2019-11-14 00:11:07 -0500

    created build

---
+++ client/build/precache-manifest.d954ab37c7422fe2c9c1847d51ee7ec8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "61564bd916bad21e74fc",
+    "url": "/static/css/main.e5e381b3.chunk.css"
+  },
+  {
+    "revision": "61564bd916bad21e74fc",
+    "url": "/static/js/main.c657f193.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "dfce50d296ab536768d3",
+    "url": "/static/js/2.26e11956.chunk.js"
+  },
+  {
+    "revision": "811d76882b036019fdef5ad11ff1ab54",
+    "url": "/static/media/sunset-1.811d7688.jpg"
+  },
+  {
+    "revision": "e7a4198404b2842f4e0afa5952dca43b",
+    "url": "/static/media/sunset1.e7a41984.JPG"
+  },
+  {
+    "revision": "ddb18602fe95fd4bb8e07f5b6aa17e47",
+    "url": "/static/media/sunset2.ddb18602.JPG"
+  },
+  {
+    "revision": "488247874e681d6a32c5757b4a1d94f6",
+    "url": "/static/media/sunset3.48824787.JPG"
+  },
+  {
+    "revision": "91992f53df9a82fa5f484315e40314e9",
+    "url": "/static/media/swirls.91992f53.png"
+  },
+  {
+    "revision": "28318d5a425c20c4238474f7080efca3",
+    "url": "/index.html"
+  }
+];
